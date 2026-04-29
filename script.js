a5046748bfda70e2ace9d488208f22d857566164
Ana Vasconcelos <bihhtuubee@gmail.com>
2026-04-29 15:08:28 -0300

    finalizando o javascript e o projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+
+function verificarPalpite() {
+  let palpite = document.getElementById("palpite").value;
+  let mensagem = document.getElementById("mensagem");
+
+  if (palpite == numeroSecreto) {
+    mensagem.innerText  = "Parabéns! Você acertou!";
+  }
+  else if (palpite < numeroSecreto) {
+    mensagem.innerText  = "Tente um número maior!";
+  }
+  else {
+    mensagem.innerText  = "Tente um número menor!";
+  }
+}
